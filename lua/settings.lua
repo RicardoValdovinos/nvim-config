675f2dd933ea7ec822d9c61d0c388c16b16a9766
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2021-10-31 00:04:09 -0700

    Uncommented indent settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,8 +11,8 @@ g.mapleader = ' '             -- change leader to a space
 opt.mouse = 'a'               -- enable mouse support
 opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
 opt.swapfile = false          -- don't use swapfile
--- opt.smartindent = true
--- opt.autoindent = true
+opt.smartindent = true
+opt.autoindent = true
 
 -----------------------------------------------------------
 -- Neovim UI
