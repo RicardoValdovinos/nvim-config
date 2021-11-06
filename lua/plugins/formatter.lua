5acfa3f238cf0114606569ee4951be7bab09f586
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2021-11-06 15:11:14 -0700

    Refactored

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,26 +1,28 @@
-require("formatter").setup(
-  {
-    filetype = {
-      javascript = {
-        -- prettier
-        function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
-            stdin = true
-          }
-        end
-      },
-      lua = {
-        -- luafmt
-        function()
-          return {
-            exe = "luafmt",
-            args = {"--indent-count", 2, "--stdin"},
-            stdin = true
-          }
-        end
-      }
+function format_prettier()
+  return {
+    exe = "prettier",
+    args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+    stdin = true
+  }
+end
+
+require("formatter").setup {
+  logging = true,
+  filetype = {
+    javascript = {format_prettier},
+    javascriptreact = {format_prettier},
+    typescript = {format_prettier},
+    typescriptreact = {format_prettier},
+    json = {format_prettier},
+    lua = {
+      -- luafmt
+      function()
+        return {
+          exe = "luafmt",
+          args = {"--indent-count", 2, "--stdin"},
+          stdin = true
+        }
+      end
     }
   }
-)
+}
