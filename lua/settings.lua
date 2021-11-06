dce87d669f7b4dfe7373c684521b6b094f8f3bed
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2021-11-06 15:12:16 -0700

    Formatted code

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,50 +1,53 @@
-local cmd = vim.cmd     				-- execute Vim commands
-local exec = vim.api.nvim_exec 				-- execute Vimscript
-local fn = vim.fn       				-- call Vim functions
-local g = vim.g         				-- global variables
-local opt = vim.opt         				-- global/buffer/windows-scoped options
+local cmd = vim.cmd -- execute Vim commands
+local exec = vim.api.nvim_exec -- execute Vimscript
+local fn = vim.fn -- call Vim functions
+local g = vim.g -- global variables
+local opt = vim.opt -- global/buffer/windows-scoped options
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
-g.mapleader = ' '             -- change leader to a space
-opt.mouse = 'a'               -- enable mouse support
-opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
-opt.swapfile = false          -- don't use swapfile
+g.mapleader = " " -- change leader to a space
+opt.mouse = "a" -- enable mouse support
+opt.clipboard = "unnamedplus" -- copy/paste to system clipboard
+opt.swapfile = false -- don't use swapfile
 opt.smartindent = true
 opt.autoindent = true
 
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true             -- show line number
-opt.showmatch = true          -- highlight matching parenthesis
-opt.foldmethod = 'marker'     -- enable folding (default 'foldmarker')
-opt.colorcolumn = '80'        -- line lenght marker at 80 columns
-opt.splitright = true         -- vertical split to the right
-opt.splitbelow = true         -- orizontal split to the bottom
-opt.ignorecase = true         -- ignore case letters when search
-opt.smartcase = true          -- ignore lowercase for the whole pattern
-opt.linebreak = true          -- wrap on word boundary
+opt.number = true -- show line number
+opt.showmatch = true -- highlight matching parenthesis
+opt.foldmethod = "marker" -- enable folding (default 'foldmarker')
+-- opt.colorcolumn = '80'        -- line lenght marker at 80 columns
+opt.splitright = true -- vertical split to the right
+opt.splitbelow = true -- orizontal split to the bottom
+opt.ignorecase = true -- ignore case letters when search
+opt.smartcase = true -- ignore lowercase for the whole pattern
+opt.linebreak = true -- wrap on word boundary
 
 -- remove whitespace on save
 cmd [[au BufWritePre * :%s/\s\+$//e]]
 
 -- highlight on yank
-exec([[
+exec(
+  [[
   augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
   augroup end
-]], false)
+]],
+  false
+)
 
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true      -- use spaces instead of tabs
-opt.shiftwidth = 4        -- shift 4 spaces when tab
-opt.tabstop = 4           -- 1 tab == 4 spaces
-opt.smartindent = true    -- autoindent new lines
+opt.expandtab = true -- use spaces instead of tabs
+opt.shiftwidth = 4 -- shift 4 spaces when tab
+opt.tabstop = 4 -- 1 tab == 4 spaces
+opt.smartindent = true -- autoindent new lines
 
 -- don't auto commenting new lines
 cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
@@ -54,16 +57,18 @@ cmd [[autocmd FileType text,markdown,xml,html,xhtml,javascript setlocal cc=0]]
 
 -- 2 spaces for selected filetypes
 cmd [[
-  autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
+  autocmd FileType xml,html,xhtml,css,scss,javascript,typescript,lua,yaml,javascriptreact,typescriptreact setlocal shiftwidth=2 tabstop=2
 ]]
 
 -- Provided by setup function
-vim.api.nvim_set_keymap('n', '<silent><leader>f', ':Format<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<silent><leader>f", ":Format<CR>", {noremap = true, silent = true})
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
 augroup FormatAutogroup
   autocmd!
   autocmd BufWritePost *.js,*.rs,*.lua FormatWrite
 augroup END
-]], true)
-
+]],
+  true
+)
