8c5cba225f2450605e799b29e582f99ac994f922
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2021-11-07 01:23:47 -0700

    Added lsp-installer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,6 +11,11 @@ return require('packer').startup(function()
   -- nvim-lspconfig
   use 'neovim/nvim-lspconfig'
 
+  -- LSP installer
+  use {
+    'williamboman/nvim-lsp-installer',
+  }
+
   -- Snippets
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
